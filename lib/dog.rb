311d6483ce16989b7fd6694de87ac1aff27f27df
mattlistor <mattlistor@gmail.com>
2019-07-17 09:05:24 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self 
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.print_all
+        @@all.length.times do |i|
+            puts @@all[i].name
+        end
+    end 
+
+    def self.clear_all
+        @@all = []
+    end 
+end
