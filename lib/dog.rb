e44abd6d7c7125a6cbbc7c740047eb078c0d6f3a
Andrew Cheon <cheonandrew113@gmail.com>
2019-07-22 09:35:15 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog
+
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize (name)
+        @name = name
+
+        Dog.all << self
+    end
+
+    #CLASS MOETHOD
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+
+        @@all.all.clear
+    end
+
+    def self.print_all
+
+        @@all.all.print
+    end
+
+
+end
